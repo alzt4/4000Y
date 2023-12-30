af1b32156ab9f5cc1f779884df37e6b77b64fe22
trent <trent@Debian2.myguest.virtualbox.org>
2023-12-30 13:44:48 -0500

    runDocker.sh now takes command line arguments

--- Docker/runDocker.sh
+++ Docker/runDocker.sh
@@ -8,26 +8,29 @@ if [ -z $1 ]; then
 	echo You forgot to supply an argument on the command line!
 	exit 1
 fi
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+STUDENTFOLDER=${SCRIPTPATH}/${1}
+TESTFOLDER=${STUDENTFOLDER}/student_module
 
-TESTFOLDER = ${1}/student_module
 
 #check if the directory exists
-if [ ! -d TESTFOLDER ]; then
+if [ ! -d ${TESTFOLDER} ]; then
 	echo Directory does not exist
 	exit 2
 fi
 
-cp unit_test.py $TESTFOLDER
-co Dockerfile $TESTFOLDER
+cp ${SCRIPTPATH}/unit_test.py ${TESTFOLDER}
+cp ${SCRIPTPATH}/Dockerfile ${STUDENTFOLDER}
 
+cd ${STUDENTFOLDER}
 docker build -t testing_image -f Dockerfile .
 docker run -it --name testing_container testing_image
 docker logs testing_container > result.txt
 docker rm -f testing_container
 docker image rm testing_image
-docker system prune -a
-
+docker container prune -f
+cd ../
 rm ${TESTFOLDER}/unit_test.py
-rm ${TESTFOLDER}/Dockerfile
+rm ${STUDENTFOLDER}/Dockerfile
 
 exit ${?}
