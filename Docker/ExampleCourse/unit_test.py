ac3b8eee9c3c3ce5eec9a667bad71809edac99b8
trent <trent@Debian2.myguest.virtualbox.org>
2024-01-13 18:55:06 -0500

    testing new custom test runner class

--- Docker/ExampleCourse/unit_test.py
+++ Docker/ExampleCourse/unit_test.py
@@ -4,19 +4,16 @@ from main import test_sum
 
 class baseUnitTest(unittest.TestCase):
     _testTimes = {}
-    
-    def testTimes(self):
-        return self._testTimes
     def setUp(self):
         self._startTime = time.time()
     def tearDown(self):
         t = time.time() - self._startTime
         self._testTimes[self.id()] = t
-        print('%s: %.3f' % (self.id(), t))
+        print('%s: %.3f\n' % (self.id(), t))
 
 
 
-class testSum(baseUnitTest):
+class testCases(baseUnitTest):
     def test_test_sum(self):
         #we expect this to pass
         self.assertEqual(test_sum(1, 2), 3, "Answer should be 3")
@@ -24,8 +21,11 @@ class testSum(baseUnitTest):
         #we expect this to fail
         self.assertEqual(test_sum(1, 3), 3, "Answer should be 3")
 
-
+class customTestRunner(unittest.TextTestRunner):
+    def _makeResult(self):
+        result = super()._makeResult()
+	super().stream.write(result.collectedDurations)
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(testSum)
-    unittest.TextTestRunner(verbosity=2).run(suite)
-    print(testSum.testTimes())
+    suite = unittest.TestLoader().loadTestsFromTestCase(testCases)
+    customTestRunner(verbosity=2).run(suite)
