ece1f782c10fdb6c2359651ac01914decda47269
alzt4 <adrianlimzhengting@trentu.ca>
2024-01-14 17:31:10 -0500

    Created custom TextTestRunner class which can output logs
    Custom TextTestRunner class which can take in any unittest TestCase or TestSuite, and automatically creates a JSON result file

--- Docker/ExampleCourse/unit_test.py
+++ Docker/ExampleCourse/unit_test.py
@@ -3,18 +3,9 @@ import time
 import json
 from main import test_sum
 
-class baseUnitTest(unittest.TestCase):
-    _testTimes = {}
-    def setUp(self):
-        self._startTime = time.time()
-    def tearDown(self):
-        t = time.time() - self._startTime
-        self._testTimes[self.id()] = t
-        print('%s: %.3f\n' % (self.id(), t))
 
 
-
-class testCases(baseUnitTest):
+class testCases(unittest.TestCase):
     def test_test_sum(self):
         #we expect this to pass
         self.assertEqual(test_sum(1, 2), 3, "Answer should be 3")
@@ -23,10 +14,10 @@ class testCases(baseUnitTest):
         self.assertEqual(test_sum(1, 3), 3, "Answer should be 3")
 
 class customTestRunner(unittest.TextTestRunner):
-    def _makeResult(self):
-        result = super()._makeResult()
+    def run(self, test):
+        result = super().run(test)
         resultDict = {
-            "successes": len(result.testsRun) - len(result.errors) - len(result.failures) - len(result.skipped) - len(result.unexpectedSuccesses),
+            "successes": result.testsRun - len(result.errors) - len(result.failures) - len(result.skipped) - len(result.unexpectedSuccesses),
             "failures": len(result.failures),
             "errors": len(result.errors),
             "skipped": len(result.skipped),
@@ -35,6 +26,9 @@ class customTestRunner(unittest.TextTestRunner):
         json_object = json.dumps(resultDict, indent=4)
         with open("result.json", "w") as outfile:
             outfile.write(json_object)
+        return result
+    
+    
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(testCases)
     customTestRunner(verbosity=2).run(suite)
