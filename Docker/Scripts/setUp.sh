748910662166dd41ee6b88a4a0532e31f9ce7dff
alzt4 <adrianlimzhengting@trentu.ca>
2024-02-23 22:07:16 -0500

    Updated to install gtest as well

--- Docker/Scripts/setUp.sh
+++ Docker/Scripts/setUp.sh
@@ -39,4 +39,13 @@ apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docke
 #jq
 apt install jq
 
+cd /
+git clone https://github.com/google/googletest.git -b v1.14.0
+cd googletest        # Main directory of the cloned repository.
+mkdir build          # Create a directory to hold the build output.
+cd build
+cmake ..             # Generate native build scripts for GoogleTest.
+make
+make install    # Install in /usr/local/ by default
+
 exit ${?}
