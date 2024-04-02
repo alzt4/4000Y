b220c9f729430d274c943dac8eec7afd75904496
Avery Chin <avery.chin@gmail.com>
2024-04-02 05:36:08 -0400

    edit assn wip

--- Server/knexfile.js
+++ Server/knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
   development: {
     client: 'mysql',
     connection: {
-        host: '192.168.0.16',
+        host: '127.0.0.1',
         port: 3306,
         user: 'root',
         password: '',
